8774f0ac5ddbf1892f7572e00c3a41713ace94e2
AiduKnow <j.s.messaoud@gmail.com>
2023-01-31 11:22:05 +0100

    test

--- src/main/java/MavenTP3/jas_poo/JasPooApplication.java
+++ src/main/java/MavenTP3/jas_poo/JasPooApplication.java
@@ -2,7 +2,7 @@ package MavenTP3.jas_poo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//TEST
 @SpringBootApplication
 public class JasPooApplication {
 
